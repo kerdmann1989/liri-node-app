218eae635e4744734c7ef06ec7edcedcc878190d
Kathy Erdmann <kathryn.erdmann@hsc.utah.edu>
2018-10-04 17:27:40 -0600

    working spotify code

--- liri.js
+++ liri.js
@@ -2,48 +2,71 @@
 require("dotenv").config();
 
 var keys = require("./keys")
-var spotifyAPI = require("./keys")
+// var spotifyAPI = require("./keys")
 var fs = require("fs");
 var request = require("request");
 var input = process.argv;
 var phrase = input[2];
-var movieName = "";
+var selection = "";
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+
 
 for (var i = 3; i < input.length; i++){
   if (i > 3 && i < input.length) {
-    movieName = movieName + "+" + input[i];
-    console.log("My Movie" + movieName);
+    selection = selection + "+" + input[i];
   } else {
-    movieName += input[i];
+    selection += input[i];
   }
 }
 
 function movie() {
-    if (movieName == null) {
-    movieName = "Mr. Nobody";
+    if (selection == null) {
+    selection = "Mr. Nobody";
 } 
 
-request("http://www.omdbapi.com/?t="+movieName+"&y=&plot=short&tomatoes=True&apikey=trilogy", function(error, response, body) {
+request("http://www.omdbapi.com/?t="+selection+"&y=&plot=short&tomatoes=True&apikey=trilogy", function(error, response, body) {
   // If the request is successful (i.e. if the response status code is 200)
   if (!error && response.statusCode === 200) {
 
-    console.log("------------------------");
-    console.log("Movie Title: " + JSON.parse(body).Title);
-    console.log("Release Year: " + JSON.parse(body).Year);
-    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-    console.log("Country: " + JSON.parse(body).Country);
-    console.log("Language: " + JSON.parse(body).Language);
-    console.log("Plot: " + JSON.parse(body).Plot);
-    console.log("Lead Actors: " + JSON.parse(body).Actors);
-    console.log("------------------------");
-}
+    console.log("\n\r");
+    console.log("==================================== OMDB MOVIE DATA ===================================="  + "\n" );
+    console.log("Movie Title: " + JSON.parse(body).Title + "\n");
+    console.log("Release Year: " + JSON.parse(body).Year + "\n");
+    console.log("IMDB Rating: " + JSON.parse(body).imdbRating + "\n");
+    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\n");
+    console.log("Country: " + JSON.parse(body).Country + "\n");
+    console.log("Language: " + JSON.parse(body).Language + "\n");
+    console.log("Plot: " + JSON.parse(body).Plot + "\n");
+    console.log("Lead Actors: " + JSON.parse(body).Actors + "\n");
+    console.log("========================================================================================"  + "\n" );}
 });
 }
 
+function spotifyThis() {
+  if (selection === null) {
+    selection == "The Sign";
+  }
+  spotify.search ({type: 'track', query: selection}, function(err, data) {
+    if (err) {
+     console.log('Error occurred: ' + err);
+     return;
+
+    } else {
+      console.log("\n\r")
+      console.log("============================== SPOTIFY ==============================" + "\n")
+      console.log("Artist: " + data.tracks.items[0].album.artists[0].name + "\n");
+      console.log("Song Title: " + data.tracks.items[0].name  + "\n");
+      console.log("Song preview: " + data.tracks.items[0].artists[0].external_urls.spotify  + "\n");
+      console.log("Album Name: " + data.tracks.items[0].album.name  + "\n");
+      console.log("=====================================================================" + "\n")
+    }
+  });
+} 
+
 switch (phrase) {
 case "spotify-this-song":
-  spotify();
+  spotifyThis();
   break;
 
 case "movie-this":
